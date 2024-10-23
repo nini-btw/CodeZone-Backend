b188d724c0c6d0be28b8081cf671656a8a264eab
nini-btw <mohamed01112002@gmail.com>
2024-10-23 12:14:19 +0100

    changing the index name

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
-const {MongoClient} = require("mongodb");
-const url ="mongodb+srv://nini_btw:justme2002@learn-mongo-db.tck39.mongodb.net/?retryWrites=true&w=majority&appName=learn-mongo-db"
-const client = new MongoClient(url);
+//Creating The Server
+require('dotenv').config();
+const httpStatusText=require('./utils/httpStatusText')
+const messages=require('./utils/messages')
+const express= require('express');
+const cors=require('cors');
+const app=express();
+const path=require('node:path'); 
 
-const main = async()=>{ 
-    //connecting to server
-    await client.connect();
-    console.log("connected");
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-    // choose database
-    const db = client.db('codeZone');
+//Connecting to The DataBase
+const mongoose=require('mongoose');
+const url=process.env.MONGO_URL;
 
-    //choose collection
-    const collection = db.collection('courses');
+mongoose.connect(url)
+.then(()=>{
+    console.log("MongoDb connect Success");
+});
 
-    //Create Query
-    await collection.insertOne({
-        title:"new Course",
-        price:5000
+
+app.use(cors());
+app.use(express.json());
+
+  
+
+//Courses Routing
+const courseRouter=require('./routes/course.route');
+app.use('/api/courses',courseRouter);
+
+//Users Routing 
+const userRouter=require('./routes/user.route');
+app.use('/api/users',userRouter);
+
+//Products Routing
+const productRouter=require('./routes/product.route');
+app.use('/api/products',productRouter);
+
+//global middleware for not found router
+app.all('*',(req,res,next)=>{
+    res.status(404).json({
+        status:httpStatusText.E,
+        data:null,
+        message:messages.EM
     })
+})
 
 
-    //Get Query
-    const data = await collection.find().toArray();
-    console.log("data ",data);
-}
+//global error handler
+app.use((error,req,res,next)=>{
+    res.status(error.statusCode || 500 ).json({
+        status: error.statusText || httpStatusText.E,
+        message:error.message,
+        code: error.statusCode || 500,
+        data:null
+    })
+});
 
-main();
-
-
-
+//Connecting To the Server
+app.listen(3000,()=>{
+    console.log('listening on PORT 3000');
+})
